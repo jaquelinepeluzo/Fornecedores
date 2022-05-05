dbfa7ab56bd3f2e47ebe6f8c3c98022798846959
Jaqueline Peluzo <jaquelinepeluzo@yahoo.com.br>
2022-05-05 15:01:13 -0300

    Criacao Model Fornecedores CPF

---
+++ Fornecedores/Models/Fornecedores.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fornecedores.Models
+{
+    public class Fornecedores
+    {
+        public int Id { get; set; }
+
+        public string TipoDePessoa { get; set; }
+
+        public string Nacional { get; set; }
+
+        public string Situacao { get; set; }
+
+        public string CPF { get; set; }
+
+        public string NomeInteiro { get; set; }
+
+        public string EstadoCivil { get; set; }
+
+        public string Profissao { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefone1 { get; set; }
+
+        public string Telefone2 { get; set; }
+
+        public string TipoDeEmpresa { get; set; }
+
+        public string Genero { get; set; }
+
+        public string Nacionalidade { get; set; }
+
+        public string Nascimento { get; set; }
+
+    }
+}
