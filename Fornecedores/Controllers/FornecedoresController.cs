db93ca4aa70f89fb7feff9681a0cae3dcf30a988
Jaqueline Peluzo <jaquelinepeluzo@yahoo.com.br>
2022-05-05 14:50:58 -0300

    Adicionar arquivos de projeto.

---
+++ Fornecedores/Controllers/FornecedoresController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fornecedores.Controllers
+{
+    public class FornecedoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
