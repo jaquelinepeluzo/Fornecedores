08d9ca00bd162f668afad7e80e4a6876bdf52a4d
Jaqueline Peluzo <jaquelinepeluzo@yahoo.com.br>
2022-05-06 10:03:54 -0300

    Criacao do banco

--- Fornecedores/Data/FornecedoresContext.cs
+++ Fornecedores/Data/FornecedoresContext.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Fornecedores.Data
 {
-    public class FornecedoresContext
+    public class FornecedoresContext : DbContext
     {
+        public FornecedoresContext(DbContextOptions<FornecedoresContext> options) : base(options){}
     }
 }
